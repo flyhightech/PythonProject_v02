2c215f5590735c38ae4fd18d01148939ead03e3e
Bernard Huff <bernard.huff008@gmail.com>
2022-06-17 21:59:50 +0000

    Playing around with arrays

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 paper = 77;
 day = 16;
+
+hat_is_on = True; 
+
+my_fav_movies = ["Training Day" , "Endgame" , "Lego Movie"];
+
 print(paper);
 print(day);
+print(hat_is_on);
+print(my_fav_movies[1]);
